936c010230cc3b7a552c14c67cfee13afdfd2a44
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-06 19:55:25 +0800

    Add repository implementation for activity service and repository

---
+++ src/ServerlessMicroservice.Infrastructure/Repositories/ActivityRepository.cs
@@ -0,0 +1,34 @@
+﻿using ServerlessMicroservice.Domain.Entities;
+using ServerlessMicroservice.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessMicroservice.Infrastructure.Repositories
+{
+    public class ActivityRepository : IActivityRepository
+    {
+        //private IDbConnection _connection { get { return new SqlConnection(_connectionString); } }
+
+        public ActivityRepository()
+        {
+                
+        }
+       
+        public Task AddAsync(Activity product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Activity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Activity> GetByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
