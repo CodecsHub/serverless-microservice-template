bcbb3aac18352de69bca425cfe676c6bf90d1e23
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-06 17:37:28 +0800

    Add entity activity class for DAO

---
+++ src/ServerlessMicroservice.Domain/Entities/Activity.cs
@@ -0,0 +1,16 @@
+﻿using ServerlessMicroservice.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerlessMicroservice.Domain.Entities
+{
+    public class Activity : BaseEntity
+    {
+        public int UserId { get; set; }
+        public int ApplicationId { get; set; }
+        public int ActionId { get; set; }
+        public string ApplicationUrl { get; set; }
+        public string ActivityRemarks { get; set; }
+    }
+}
