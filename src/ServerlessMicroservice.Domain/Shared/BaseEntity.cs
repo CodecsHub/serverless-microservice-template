7e7a393627d38223ac8dd677b5f19c572ac22328
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-06 17:15:24 +0800

    Add shared entity for generic data list

---
+++ src/ServerlessMicroservice.Domain/Shared/BaseEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerlessMicroservice.Domain.Shared
+{
+    public abstract class BaseEntity
+    {
+        private DateTime _dateCreated;
+        private DateTime _dateUpdated;
+
+
+        public Int64 Id { get; set; }
+        public int RowVersionNumber { get; set; }
+        public DateTime DateTimeLog { get; protected set; } = DateTime.UtcNow;
+
+        public DateTime DateCreated
+        {
+            get => _dateCreated;
+            set => _dateCreated = DateTimeLog;
+        }
+
+        public DateTime DateUpdated
+        {
+            get => _dateUpdated;
+            set => _dateUpdated = DateTimeLog;
+        }
+    }
+}
