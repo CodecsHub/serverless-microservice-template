a65dd81bfd12fb1a4e558fd18924977a8a7d478a
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-06 17:38:20 +0800

    Add entity user class for DAO

---
+++ src/ServerlessMicroservice.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using ServerlessMicroservice.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerlessMicroservice.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string AzureId { get; set; }
+        public string UserName { get; set; }
+    }
+}
