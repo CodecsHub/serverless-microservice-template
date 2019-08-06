c84b06384e3f8d75abc6d289104477c6a3df4447
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-06 23:02:17 +0800

    Add  httpsredirection middleware pipeline for security configuration

--- src/ServerlessMicroservice.API/Startup.cs
+++ src/ServerlessMicroservice.API/Startup.cs
@@ -90,9 +90,13 @@ namespace ServerlessMicroservice.API
             }
             else
             {
+                // The default HSTS value is 30 days. 
+                //You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            // force the request to use https traffic
+            app.UseHttpsRedirection();
             app.UseMvc();
 
         }
