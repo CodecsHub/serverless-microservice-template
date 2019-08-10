82ccc8b45518d2b16d8cfa6d806833fb5503abea
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-11 00:33:33 +0800

    Add Autofac v4.9.3 nugget library for tracking easy the dependency injuection

---
+++ tests/ServerlessMicroservice.Test/Service/ActivityServiceTest.cs
@@ -0,0 +1,82 @@
+﻿using ServerlessMicroservice.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerlessMicroservice.Test.Service
+{
+    public class ActivityServiceTest
+    {
+        [Fact]
+        public void LoadPeople_ValidCall()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<ISqliteDataAccess>()
+                    .Setup(x => x.LoadData<PersonModel>("select * from Person"))
+                    .Returns(GetSamplePeople());
+
+                var cls = mock.Create<Activity>();
+                var expected = GetSamplePeople();
+
+                var actual = cls.LoadPeople();
+
+                Assert.True(actual != null);
+                Assert.Equal(expected.Count, actual.Count);
+
+                for (int i = 0; i < expected.Count; i++)
+                {
+                    Assert.Equal(expected[i].FirstName, actual[i].FirstName);
+                    Assert.Equal(expected[i].LastName, actual[i].LastName);
+                }
+            }
+        }
+
+
+
+        private List<Activity> GetSamplePeople()
+        {
+            List<Activity> output = new List<Activity>
+            {
+                new Activity
+                {
+                    Id = 1,
+                    UserId = 1,
+                    ApplicationId = 1,
+                    ActionId =1,
+                    ApplicationUrl = "https",
+                    ActivityRemarks = "code just copy"
+                },
+                new Activity
+                {
+                   Id = 2,
+                    UserId = 1,
+                    ApplicationId = 1,
+                    ActionId =1,
+                    ApplicationUrl = "https",
+                    ActivityRemarks = "code just copy"
+                },
+                new Activity
+                {
+                   Id = 3,
+                    UserId = 2,
+                    ApplicationId = 2,
+                    ActionId =2,
+                    ApplicationUrl = "https",
+                    ActivityRemarks = "code just copy"
+                },
+                new Activity
+                {
+                   Id = 4,
+                    UserId = 3,
+                    ApplicationId = 3,
+                    ActionId =2,
+                    ApplicationUrl = "https only",
+                    ActivityRemarks = "code just copy"
+                }
+            };
+
+            return output;
+        }
+    }
+}
