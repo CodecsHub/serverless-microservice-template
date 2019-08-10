913c2caa1b3b3341868340077517b7b2a38cc9b6
cocoytech <i.am.cocoytech+github@gmail.com>
2019-08-10 22:43:31 +0800

    Add ValidationFIlterAttribute class as helper for extended function and refactored controller API request

---
+++ src/ServerlessMicroservice.API/Helpers/ValidationFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerlessMicroservice.API.Helpers
+{
+    public class ValidationFilterAttribute : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState); // returns 400 with error
+            }
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
